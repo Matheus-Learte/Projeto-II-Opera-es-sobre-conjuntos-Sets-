651af56c42d0244b5e7a2680a00ec507fe452496
Math <matheuslearth@gmail.com>
2024-12-05 10:55:19 -0300

    Faltando comentar

--- LLBRBB/LLRBT.h
+++ LLBRBB/LLRBT.h
@@ -5,11 +5,11 @@
 
     typedef struct llrbt LLRBT;
 
-    LLRBT* llrbt_criar();//*
-    void llrbt_apagar(LLRBT** T);//*
-    int llrbt_buscar(LLRBT* T, int chave);//*
+    LLRBT* llrbt_criar();
+    void llrbt_apagar(LLRBT** T);
+    bool llrbt_buscar(LLRBT* T, int chave);
     bool llrbt_inserir(LLRBT* T, int chave);
-    void llrbt_apagar(LLRBT* T, int chave);
-    void llrbt_imprimir(LLRBT* T);//*
+    void llrbt_remover(LLRBT* T, int chave);
+    void llrbt_imprimir(LLRBT* T);
 
 #endif
