469edf8d84dedc37c26ece6147d337d16575e02b
Dr-Verdin <piscioneri24122004@gmail.com>
2024-12-10 00:28:22 -0300

    LGBT.h comentado

--- LLRBT.h
+++ LLRBT.h
@@ -1,17 +1,21 @@
+ /*
+ O cabeçalho de um módulo em C para manipulação de uma árvore Rubro-Negra (Left-Learning Red-Black Tree), que é uma árvore binária de busca balanceada:
+ */ 
+
 #ifndef RBT_H
     #define RBT_H
 
-    #include <stdbool.h>
+    #include <stdbool.h> // Biblioteca para manipulação de booleanos.
 
-    typedef struct llrbt LLRBT;
+    typedef struct llrbt LLRBT; // Defnição do tipo 'LLRBT'.
 
-    LLRBT* llrbt_criar();
-    void llrbt_apagar(LLRBT** T);
-    bool llrbt_buscar(LLRBT* T, int chave);
-    bool llrbt_inserir(LLRBT* T, int chave);
-    bool llrbt_remover(LLRBT* T, int chave);
-    void llrbt_imprimir(LLRBT* T);
-    LLRBT* llrbt_uniao(LLRBT* T, LLRBT* C);
-    LLRBT* llrbt_interseccao(LLRBT* T, LLRBT* C);
+    LLRBT* llrbt_criar(); // Função para criar uma árvore LLRBT.
+    void llrbt_apagar(LLRBT** T); // Função para apagar a árvore.
+    bool llrbt_buscar(LLRBT* T, int chave); // Função para buscar um elemento na árvore.
+    bool llrbt_inserir(LLRBT* T, int chave); // Função para inserir um elemento na árvore.
+    bool llrbt_remover(LLRBT* T, int chave); // Função para remover um elemento da árvore.
+    void llrbt_imprimir(LLRBT* T); // Função para imprimir os elementos, em ordem crescente, da árvore.
+    LLRBT* llrbt_uniao(LLRBT* T, LLRBT* C); // Função para realizar a união de duas árvores.
+    LLRBT* llrbt_interseccao(LLRBT* T, LLRBT* C); // Função para realizar a intersecção de duas árvores.
 
 #endif
